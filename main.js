6877c5848bc3ee951a407ae012d783e0b3befc1f
Jonathan Arreola <jonathan.arreola@zoro.com>
2022-10-24 13:23:29 -0400

    added event handling v-on or @ listeners and event firing

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = Vue.createApp({
         return {
             cart:0,
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
+            image: './assets/images/socks_green.jpg',
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
@@ -11,5 +11,18 @@ const app = Vue.createApp({
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
+    },
+    methods: {
+      addToCart() {
+        this.cart +=1
+      },
+      removeFromCart() {
+        if (this.cart > 0) {
+          this.cart -=1
+        }
+      },
+      updateImage(variantImage) {
+        this.image = variantImage
+      }
     }
 })
