69514b2606a38e6cf033bf8d2bfa52b67201c621
Jonathan Arreola <jonathan.arreola@zoro.com>
2022-10-24 12:16:15 -0400

    my solution to L2

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  // data: function () {
+  //   return {
+  //     product: 'Socks'
+  //   }
+  // }
+
+    data() {
+    return {
+      product: {
+        name: 'Socks',
+        description: 'These are the socks available from Vue'
+      }
+    }
+  }
+
+})
